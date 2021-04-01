f25775067c3ec240ad191279bcda6d55ffb192f8
letyhashimoto <67491329+letyhashimoto@users.noreply.github.com>
2021-04-01 20:53:09 -0300

    Delete Aula 01 Conversor de Moedas directory

--- Aula 01 Conversor de Moedas/app.js
+++
@@ -1,6 +0,0 @@
-function ieneConverter(valor){
-	let valorIene = parseFloat(valor); 
-	let valorReal = valorIene * 0.05;
-	
-	document.getElementById("valorReal").innerHTML = valorReal.toFixed(2);
-}
